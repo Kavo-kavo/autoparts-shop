83579819517d23a063ac950e00b2f65c3c099a98
ktyf <63629662+Kavo-kavo@users.noreply.github.com>
2025-12-10 03:01:11 +0300

    Add files via upload

--- backend/database.py
+++ backend/database.py
@@ -3,13 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1212@localhost/autoparts_db")
-##SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1212@localhost/autoparts_db"
+DB_URL = os.getenv("POSTGRES_URL") or os.getenv("DATABASE_URL") or "postgresql://postgres:1212@localhost/autoparts_db"
 
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DB_URL.startswith("postgres://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL)
+connect_args = {}
+if "vercel-storage" in DB_URL or "neon.tech" in DB_URL:
+    connect_args = {"sslmode": "require"}
+
+engine = create_engine(DB_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
